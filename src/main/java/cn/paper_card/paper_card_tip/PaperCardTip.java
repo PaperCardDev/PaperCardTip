d63650f72855fcd7e512b4f2a1f6de2722a0266e
Paper99 <1398872296@qq.com>
2023-10-19 13:49:18 +0800

    创建项目。

---
+++ src/main/java/cn/paper_card/paper_card_tip/PaperCardTip.java
@@ -0,0 +1,16 @@
+package cn.paper_card.paper_card_tip;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PaperCardTip extends JavaPlugin implements PaperCardTipApi {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/cn/paper_card/paper_card_tip/PaperCardTipApi.java
@@ -0,0 +1,4 @@
+package cn.paper_card.paper_card_tip;
+
+public interface PaperCardTipApi {
+}
